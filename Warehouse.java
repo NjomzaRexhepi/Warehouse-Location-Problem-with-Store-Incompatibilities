62addae2ce6d21400fb5a3eb1f92acd9061f936d
NjomzaRexhepi <njomzaarexhepii@gmail.com>
2025-03-19 18:50:30 +0100

    warehouse class

--- Warehouse.java
+++ Warehouse.java
@@ -1,2 +1,13 @@
 public class Warehouse {
+    int id;
+    int capacity;
+    int openingCost;
+    boolean isOpen;
+
+    public Warehouse(int id, int capacity, int openingCost) {
+        this.id = id;
+        this.capacity = capacity;
+        this.openingCost = openingCost;
+        this.isOpen = false;
+    }
 }
