9d561888e71ecf69ccd9cfe15472cf8b28ce17c5
NjomzaRexhepi <njomzaarexhepii@gmail.com>
2025-03-19 19:03:29 +0100

    models added

--- problem/src/models/StoreClass.java
+++ problem/src/models/StoreClass.java
@@ -1,4 +1,20 @@
 package problem.src.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class StoreClass {
+    int id;
+    int demand;
+    Set<Integer> incompatibleStores;
+
+    public StoreClass(int id, int demand) {
+        this.id = id;
+        this.demand = demand;
+        this.incompatibleStores = new HashSet<>();
+    }
+
+    public void addIncompatibleStore(int storeId) {
+        incompatibleStores.add(storeId);
+    }
 }
