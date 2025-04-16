0de7fb5de0b9cce856509083138d599d08fe2d7f
NjomzaRexhepi <njomzaarexhepii@gmail.com>
2025-04-16 18:12:26 +0200

    Solver

--- problem/src/Main.java
+++ problem/src/Main.java
@@ -3,14 +3,16 @@ package problem.src;
 import problem.src.models.InstanceData;
 import problem.src.parser.MiniZincParser;
 import problem.src.solutions.InitialSolution;
+import problem.src.solutions.Solver;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         InstanceData instance = MiniZincParser.parseFile("problem/src/inputs/input1.txt");
-        InitialSolution solution = new InitialSolution();
-        solution.generateInitialSolution(instance);
-        System.out.println(solution);
+//        InitialSolution solution = new InitialSolution();
+//        solution.generateInitialSolution(instance);
+//        System.out.println(solution);
+        Solver.solver(instance);
     }
 }
--- problem/src/solutions/Solver.java
+++ problem/src/solutions/Solver.java
@@ -4,6 +4,10 @@ import problem.src.models.InstanceData;
 import problem.src.models.StoreClass;
 import problem.src.models.WarehouseClass;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Solver {
@@ -36,10 +40,20 @@ public class Solver {
 
             boolean assignedFully = false;
             for (int j = 0; j < nWarehouses; j++) {
-               //TODO
+                if (remainingCapacity[j] >= remainingDemand && canAssignStore(i, warehouseAssignedStores.get(j), incompatiblePairs)) {
+
+                    solutionMatrix[i][j] = remainingDemand;
+                    remainingCapacity[j] -= remainingDemand;
+                    warehouseAssignedStores.get(j).add(i);
+                    assignedFully = true;
+                    break;
+                }
             }
             if (!assignedFully) {
                 for (int j = 0; j < nWarehouses && remainingDemand > 0; j++) {
+                    if (!canAssignStore(i, warehouseAssignedStores.get(j), incompatiblePairs)) {
+                        continue;
+                    }
                     if (remainingCapacity[j] > 0) {
                         int assignQuantity = Math.min(remainingCapacity[j], remainingDemand);
                         solutionMatrix[i][j] = assignQuantity;
@@ -53,6 +67,52 @@ public class Solver {
                     System.out.println("Warning: Could not satisfy demand for store " + (i + 1));
                 }
             }
+        }
+
+        writeSolution(solutionMatrix, nStores, nWarehouses);
+    }
+
+    private static boolean canAssignStore(int storeIndex, Set<Integer> assignedStores, List<int[]> incompatiblePairs) {
+        int store1Based = storeIndex + 1;
+        for (int assignedStore : assignedStores) {
+            int assignedStore1Based = assignedStore + 1;
+            for (int[] pair : incompatiblePairs) {
+
+                if ((pair[0] == store1Based && pair[1] == assignedStore1Based) ||
+                        (pair[0] == assignedStore1Based && pair[1] == store1Based)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void writeSolution(int[][] solutionMatrix, int nStores, int nWarehouses) {
+        File outputDir = new File("problem/src/outputs");
+
+        File outputFile = new File(outputDir, "solution.txt");
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile))) {
+            bw.write("[\n");
+            for (int i = 0; i < nStores; i++) {
+                bw.write("(");
+                for (int j = 0; j < nWarehouses; j++) {
+                    bw.write(String.valueOf(solutionMatrix[i][j]));
+                    if (j < nWarehouses - 1) {
+                        bw.write(",");
+                    }
+                }
+                bw.write(")");
+                if (i < nStores - 1) {
+                    bw.write("\n");
+                }
+            }
+            bw.write("\n]");
+            bw.flush();
+            System.out.println("Solution saved to: " + outputFile.getAbsolutePath());
+        } catch (IOException e) {
+            System.err.println("Error writing solution file: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
